03d00443e34a1c3e60e73e6b97b0f20323347fd0
jayanneglalanan <jayanneglalanan@gmail.com>
2026-08-19 21:12:03 +0800

    caret

--- mobileapp/src/features/login/FloatingInput.tsx
+++ mobileapp/src/features/login/FloatingInput.tsx
@@ -86,6 +86,7 @@ export default function FloatingInput({
             autoCapitalize={autoCapitalize}
             autoCorrect={false}
             keyboardType={keyboardType}
+            caretHidden
           />
           {toggle ? (
             <Pressable
